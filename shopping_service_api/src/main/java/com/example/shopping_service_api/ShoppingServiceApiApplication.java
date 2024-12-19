05d5d7eab6559a035e7333a5d262065280e214f8
Duong191124 <119329339+Duong191124@users.noreply.github.com>
2024-12-19 14:34:34 +0700

    initialize project

---
+++ shopping_service_api/src/main/java/com/example/shopping_service_api/ShoppingServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingServiceApiApplication.class, args);
+	}
+
+}
